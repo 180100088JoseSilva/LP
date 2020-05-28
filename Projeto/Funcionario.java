69a12e5a705df156efacb42a22ed8c3d1ba372d0
180100088JoseSilva <180100088@esg.ipsantarem.pt>
2020-05-28 12:13:45 +0100

    Update Funcionario.java

--- Projeto/Funcionario.java
+++ Projeto/Funcionario.java
@@ -1,40 +1,99 @@
 package NfilhosArvore;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+
 
 public class Funcionario {
 	//Atributos
-	private String tarefa;
-	private ArrayList<Funcionario> filhosNos = new ArrayList<Funcionario>();
+	private int id;
+	private String nome;
+	private Tarefa inicial;
+	private Tarefa ultima;
+	private int quantasTarefasFaltam;
+	private ArrayList<Funcionario> subordinados = new ArrayList<Funcionario>();
 	
 	//Construtores
-	public Funcionario(){
-		this.tarefa="";
-		this.filhosNos = null;
+	public Funcionario() {
+		this.id =0;
+		this.nome ="";
+		this.inicial=null;
+		this.ultima=null;
+		this.subordinados=null;
 	}
-	public Funcionario(String c){
-		this.tarefa=c;
+	
+	public Funcionario(int id, String nome) {
+		this.id=id;
+		this.nome=nome;
+		this.inicial=null;
+		this.ultima=null;
+		this.subordinados=null;
 	}
 	
 	//Acessores
-	public String getConteudo() {
-		return tarefa;
+	public int getId() {
+		return id;
 	}
 
-	public void setConteudo(String conteudo) {
-		this.tarefa = conteudo;
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public ArrayList<Funcionario> getFilhosNos() {
-		return filhosNos;
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
-	public void addFilho(Funcionario novo) {
-	    filhosNos.add(novo);
+	//metodos
+	
+	public void addFuncionario(Funcionario novo) {
+	    subordinados.add(novo);
 	}
-	@Override
-	public String toString() {
-		return "No [conteudo=" + tarefa + ", filhosNos=" + filhosNos + "]";
+	
+	public void addTarefa(int i) {
+		// TODO Auto-generated method stub
+		Tarefa nova = new Tarefa();
+		if(estaVazia()) {
+			inicial = nova;
+			ultima = nova;
+		}
+		else {
+			ultima.setProximo(nova);
+			ultima = nova;
+		}
+		quantasTarefasFaltam++;
 	}
-
+	
+	public String obterPrimeiraTarefa() {
+		// TODO Auto-generated method stub
+		if(estaVazia()) {
+			throw new EmptyStackException();
+		}else {
+			return inicial.getTarefa();
+		}
+	}
+	
+	public String terminarTarefa() {
+		// TODO Auto-generated method stub
+		if(estaVazia()) {
+			throw new EmptyStackException();
+		}else {
+			String sair = inicial.getTarefa();
+			inicial = inicial.getProximo();
+			quantasTarefasFaltam--;
+			return sair;
+		}
+		
+	}
+	
+	public boolean estaVazia() {
+		// TODO Auto-generated method stub
+		return quantasTarefasFaltam == 0;
+	}
+	
+	
 }
