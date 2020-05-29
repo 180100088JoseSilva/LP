77c759256edd025f5e35d2420fa88e76d8531d3d
180100088JoseSilva <180100088@esg.ipsantarem.pt>
2020-05-29 19:49:53 +0100

    Update Main.java

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -1,4 +1,4 @@
-package NfilhosArvore;
+package Projeto;
 
 import java.util.Arrays;
 
@@ -6,12 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArvoreNNos t = new ArvoreNNos();
+		Empresa t = new Empresa();
 		
-		Funcionario t1 = t.inserir("Ola");
-		t.inserir("Gente", t1);
+		Funcionario t1 = new Funcionario(1, "Ze", "Patrao");
+		Funcionario t2 = new Funcionario(2, "Zes", "Mini-Patrao");
+		Funcionario t3 = new Funcionario(3, "Zess", "Gerente");
+		Funcionario t4 = new Funcionario(4, "Zesss", "Gerente");
+		
+		t.inserir(t1, null);
+		t.inserir(t2, t.procurar(1));
+		t.inserir(t3, t.procurar(2));
+		t.inserir(t4, t.procurar(2));
 		
 		
+		System.out.println(t.procurar(3).getNome());
 		
 		System.out.println(Arrays.toString(t.preOrder()));
 	}
